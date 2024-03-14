f1be9cb8719fe98375bbe611d05112e2216a6655
Lucas Jácome <104047219+lucasjacome@users.noreply.github.com>
2024-03-13 21:53:47 -0300

    Add files via upload

--- src/app/DataSet.java
+++ src/app/DataSet.java
@@ -1,5 +1,19 @@
 package app;
 
-public class DataSet {
-
-}
+public class Dataset {
+    public DataPoint[] getDataPoints() {
+        // retorna o vetor com os dados carregados 
+   
+    }
+    public int getLength() { 
+        // retorna a quantidade de pontos carregados 
+    }
+    
+    public Dataset() { 
+        // Implementar construtor 
+    } 
+    
+    public void loadDataFromCSV(String filename) { 
+        // Implementação para ler dados do arquivo CSV e carregar na lista de pontos de dados 
+    } 
+} 
--- src/app/Knn.java
+++ src/app/Knn.java
@@ -1,5 +1,18 @@
-package app;
-
 public class Knn {
+    private Dataset dataset;
+    private int k;
 
+    public Knn(Dataset dataset, int k) {
+        this.dataset = dataset;
+        this.k = k;
+    }
+
+    public String classify(DataPoint attributes) {
+        // Implementação do algoritmo K-NN para classificação
+        // Retorna a classe prevista para o ponto de dados fornecido
+    }
+
+    public String determineHappiness(double hoursOfSleep, double calories) {
+        return classify(attributes);
+    }
 }
